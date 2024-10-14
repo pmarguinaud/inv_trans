d77eefaf4dda7e034b96a9c2da9895645a2b3c76
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-10-14 18:47:47 +0000

    Use FIELD_BASIC lists

--- inv_trans.sh
+++ inv_trans.sh
@@ -5,12 +5,13 @@ module load gcc 2>/dev/null
 
 set -x
 
-for f90 in ifort gfortran ifort pgf90
+for f90 in gfortran ifort pgf90
 do
   echo "==> $f90 <=="
 
   x=inv_trans.$f90.x
   $f90 -o inv_trans.$f90.x -g -O0 inv_trans.F90
+  exit
   ./$x
 
 done
